b2472e3f71ee6a7606481890876f8c793bdd0d86
MountainEnjoyer <chauve.zakary@gmail.com>
2026-06-16 15:56:48 +0200

    Working on the scales

--- SolarSystem.c
+++ SolarSystem.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <raylib.h>
 
-#define WIDTH 1000
+#define WIDTH 1800
 #define HEIGHT 1000
 #define FPS 60
 
@@ -21,7 +21,7 @@ void InitPlanet() {
       case 0:
         Corps[i].dsun = 0;
         Corps[i].mass = 1.989*powf(10,30);
-        Corps[i].radius = (1400000/2);
+        Corps[i].radius = (1400000/10000);
         Corps[i].x = WIDTH/2;
         Corps[i].y = HEIGHT/2;
         Corps[i].vx = 0;
@@ -33,8 +33,8 @@ void InitPlanet() {
       case 1:
         Corps[i].dsun = 57.9*powf(10,6);
         Corps[i].mass = 3.3*powf(10,23);
-        Corps[i].radius = (4880/2);
-        Corps[i].x = Corps[0].x + Corps[i].dsun;
+        Corps[i].radius = (4880/1000);
+        Corps[i].x = (Corps[0].x + Corps[0].radius + (Corps[i].dsun/10000000) + Corps[i].radius);
         Corps[i].y = HEIGHT/2;
         Corps[i].vx = 0;
         Corps[i].vy = 0;
@@ -45,8 +45,8 @@ void InitPlanet() {
       case 2:
         Corps[i].dsun = 108.2*powf(10,6);
         Corps[i].mass = 4.87*powf(10,24);
-        Corps[i].radius = (12104/2);
-        Corps[i].x = Corps[0].x + Corps[i].dsun;
+        Corps[i].radius = (12104/2000);
+        Corps[i].x =  (Corps[0].x + Corps[0].radius + (Corps[i].dsun/10000000) + Corps[i].radius);
         Corps[i].y = HEIGHT/2;
         Corps[i].vx = 0;
         Corps[i].vy = 0;
@@ -57,8 +57,8 @@ void InitPlanet() {
       case 3:
         Corps[i].dsun = 149.6*powf(10,6);
         Corps[i].mass = 5.97*powf(10,24);
-        Corps[i].radius = (12756/2);
-        Corps[i].x = Corps[0].x + Corps[i].dsun;
+        Corps[i].radius = (12756/200);
+        Corps[i].x =  (Corps[0].x + Corps[0].radius + (Corps[i].dsun/10000000) + Corps[i].radius);
         Corps[i].y = HEIGHT/2;
         Corps[i].vx = 0;
         Corps[i].vy = 0;
@@ -69,8 +69,8 @@ void InitPlanet() {
       case 4:
         Corps[i].dsun = 227.9*powf(10,6);
         Corps[i].mass = 6.42*powf(10,23);
-        Corps[i].radius = (6792/2);
-        Corps[i].x =  Corps[0].x + Corps[i].dsun;
+        Corps[i].radius = (6792/200);
+        Corps[i].x =   (Corps[0].x + Corps[0].radius + (Corps[i].dsun/10000000) + Corps[i].radius);
         Corps[i].y = HEIGHT/2;
         Corps[i].vx = 0;
         Corps[i].vy = 0;
@@ -81,8 +81,8 @@ void InitPlanet() {
       case 5:
         Corps[i].dsun = 778.5*powf(10,6);
         Corps[i].mass = 1.9*powf(10,27);
-        Corps[i].radius = (142984/2);
-        Corps[i].x =  Corps[0].x + Corps[i].dsun;
+        Corps[i].radius = (142984/200);
+        Corps[i].x =   (Corps[0].x + Corps[0].radius + (Corps[i].dsun/10000000) + Corps[i].radius);
         Corps[i].y = HEIGHT/2;
         Corps[i].vx = 0;
         Corps[i].vy = 0;
@@ -93,8 +93,8 @@ void InitPlanet() {
       case 6:
         Corps[i].dsun = 1430*powf(10,6);
         Corps[i].mass = 5.88*powf(10,28);
-        Corps[i].radius = (120536/2);
-        Corps[i].x =  Corps[0].x + Corps[i].dsun;
+        Corps[i].radius = (120536/200);
+        Corps[i].x =   (Corps[0].x + Corps[0].radius + (Corps[i].dsun/10000000) + Corps[i].radius);
         Corps[i].y = HEIGHT/2;
         Corps[i].vx = 0;
         Corps[i].vy = 0;
@@ -105,8 +105,8 @@ void InitPlanet() {
       case 7:
         Corps[i].dsun = 2870*powf(10,6);
         Corps[i].mass = 8.68*powf(10,25);
-        Corps[i].radius = (51118/2);
-        Corps[i].x =  Corps[0].x + Corps[i].dsun;
+        Corps[i].radius = (51118/200);
+        Corps[i].x =  (Corps[0].x + Corps[0].radius + (Corps[i].dsun/10000000) + Corps[i].radius);
         Corps[i].y = HEIGHT/2;
         Corps[i].vx = 0;
         Corps[i].vy = 0;
@@ -117,8 +117,8 @@ void InitPlanet() {
       case 8:
         Corps[i].dsun = 4490*powf(10,6);
         Corps[i].mass = 1.02*powf(10,26);
-        Corps[i].radius = (49528/2);
-        Corps[i].x =  Corps[0].x + Corps[i].dsun;
+        Corps[i].radius = (49528/200);
+        Corps[i].x = (Corps[0].x + Corps[0].radius + (Corps[i].dsun/10000000) + Corps[i].radius);
         Corps[i].y = HEIGHT/2;
         Corps[i].vx = 0;
         Corps[i].vy = 0;
